e4d3ef35d2981775ccf2c74e3e97f1cecc280d9b
enricop <enrico.papi@gmail.com>
2016-12-22 18:56:30 +0100

    m2: add template binding example

--- m2/app/app.component.ts
+++ m2/app/app.component.ts
@@ -11,10 +11,33 @@ export class AppComponent {
     public indicator : boolean = true;
     public messages : string[] = ['Alpha', 'Bravo', 'Charlie'];
     public title : string = 'Template Binding';
+
+    public showPanel : boolean = true;
+    public getStyles() {
+		return {
+    		"font-style": "italic",
+    		"font-weight": "bold",
+		}
+    }
+
+    public getClasses() {
+		return {
+			"highlight": true,
+			"strike": false,
+		}
+    }
     
     public message : string;
     constructor() {
         let generator = new TextGenerator();
         this.message = generator.GetText();
     }
+}
+
+@Component({
+  selector: 'data-entry',
+  template: 'app/app.component.html'
+})
+export class DataEntryComponent {
+  public name : string = "John Doe";
 }
